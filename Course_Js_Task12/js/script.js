6e9a00359f100d28cf1f464557c2e9962020b7d2
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-14 00:29:25 +0300

    Add files via upload

---
+++ Course_Js_Task12/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let birthYear = prompt('What year were you born?');
+let city = prompt("What city do you live in?");
+let sport = prompt("What is your favorite sport?");
+
+let resultCity;
+let resultSport;
+let resultbirthYear;
+
+let age = 2022 - birthYear;
+
+let cancel;
+
+
+resultCity = (city === "Kiev") ? 'Kyiv is the capital of Ukraine' :
+  (city === "Moscow") ? 'Moscow is the capital of Russia' :
+    (city === "Minsk") ? 'Minsk is the capital of Belarus' :
+      `${city}`;
+  
+resultSport = (sport === "football") ? "football - Cool! Do you want to be like Ronaldo?" :
+  (sport === "Hockey") ? "Hockey - Cool! Do you want to be Patrick Kane?" :
+    (sport === "F1") ? "F1 - Cool! Do you want to be Schumacher?" :
+        `${sport} Сool choice!`;
+
+
+cancel = (birthYear === null) ? alert('You canceled the age') :
+  (city === null) ? alert('You canceled the city') :
+    (sport === null) ?  alert('You canceled the sport') :
+        alert(`Your age - ${age}: Your city - ${resultCity}: Your favorit sport - ${resultSport} `);
